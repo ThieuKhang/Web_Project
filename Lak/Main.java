831e157a31f02ff9ce9489c527fb642ecd335232
ThieuKhang <165531664+ThieuKhang@users.noreply.github.com>
2024-11-08 09:16:09 +0700

    move some files

---
+++ Lak/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create a new Person object
+        Person person = new Person("12345", "John Doe", "john.doe@example.com", "12/06/2000");
+
+        // Display all attributes
+        person.displayAttributes();
+    }
+}
